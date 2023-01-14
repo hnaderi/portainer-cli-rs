6708bb45fe65da34341d7da06a4843756a4bfb02
Hossein Naderi <hossein-naderi@hotmail.com>
2023-01-14 18:59:15 +0330

    initial commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
+  outputs = { self, nixpkgs }:
+    let pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in {
+      devShell.x86_64-linux = with pkgs;
+        mkShell {
+          name = "portainer-cli";
+          nativeBuildInputs = [ rustc cargo gcc ];
+          buildInputs = [ rustfmt rust-analyzer clippy rustup ];
+
+          RUST_SRC_PATH =
+            "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+    };
+}
